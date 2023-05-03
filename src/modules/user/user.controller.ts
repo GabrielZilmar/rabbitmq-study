c7df910c6050ec5eb9ca3287aacc192730c80cf3
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-05-02 22:55:14 -0300

    feat: add validator for create user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -9,16 +9,17 @@ import {
   Res,
 } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
-import { IUser, IUserCreate } from '~/modules/user/types';
+import { IUser } from '~/modules/user/types';
 import { UserServices } from '~/modules/user/user.service';
 import { Response } from 'express';
+import UserDto from '~/modules/user/user.dto';
 
 @Controller('/api')
 export class UserController {
   constructor(private readonly appService: UserServices) {}
 
   @Post('/users')
-  async createUser(@Body() user: IUserCreate): Promise<IUser> {
+  async createUser(@Body() user: UserDto): Promise<IUser> {
     return this.appService.createUser(user);
   }
 
--- src/modules/user/user.dto.ts
+++ src/modules/user/user.dto.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { IReqresUser, IUser } from '~/modules/user/types';
 
 @Injectable()
 export default class UserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  avatarUrl: string;
+
   public toDto({
     data: { id, email, first_name, last_name, avatar },
   }: IReqresUser): IUser {
