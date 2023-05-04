c48c36ea6dd5d14d57a0625a6d89bbed1096e8dc
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-05-03 21:18:29 -0300

    fix: eslint fix on test file

--- README.md
+++ README.md
@@ -30,6 +30,7 @@
 
 ```bash
 $ npm install
+$ docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.11-management
 ```
 
 ## Running the app
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -14,15 +13,4 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-
-  // Create user -> Check event when created user and check on db -> Check if email has been sent
-  // Create duplicated user
-
-  // Get user -> Create then get (use Dto)
-
-  // Get image from avatar
-  // Check if is getting existent user
-  // Check if plain image is saved on db and on /tmp
-
-  // Delete user and see on db and see if plain image is deleted
 });
